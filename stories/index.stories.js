f3ed96cb7756fc9cc439b605604fd0ef06e28613
Alexandre Girard <git@alexgirard.com>
2020-02-14 11:08:11 +0100

    fix: split storybook stories to avoid state issue
    Stories using a state were creating an issue on non-stated stories.
    
    Split various stories in different scripts.

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -17,31 +17,7 @@ import segmentationMasksJson from './static/segmentationMasks.json';
 import segmentationMasksBoxesJson from './static/segmentationMasksBoxes.json';
 import boxesBorderZeroJson from './static/boxesBorderZero.json';
 
-import { withState, Store } from '@sambego/storybook-state';
-
-const store = new Store({
-  index: 0,
-  images: [
-    demoImage,
-    demoImageLarge,
-    demoImageDog
-  ],
-  boxes: [
-    [
-      [0, 0, 250, 250]
-    ],
-    [
-      [50, 50, 250, 250]
-    ],
-    [
-      [100, 100, 250, 250]
-    ]
-  ]
-});
-
 storiesOf('Boundingbox', module)
-  .addDecorator(withState())
-  .addParameters({ state: { store } })
   .add('default view', () => {
     const params = {
       image: demoImageLarge,
@@ -160,21 +136,4 @@ storiesOf('Boundingbox', module)
       image={demoImageDog}
       boxes={boxesBorderZeroJson}
     />);
-  })
-  .add('redraw image', () => state => [
-      <button key='btn-demoImage' onClick={() => {
-        store.set({index: 0})
-      }}>demoImage</button>,
-      <button key='btn-demoImageLarge' onClick={() => {
-        store.set({index: 1})
-      }}>demoImageLarge</button>,
-      <button key='btn-demoImageDog' onClick={() => {
-        store.set({index: 2})
-      }}>demoImageDog</button>,
-      <p key='current-index'>current index: {state.index}</p>,
-      <Boundingbox
-        key='bounding-box'
-        image={state.images[state.index]}
-        boxes={state.boxes[state.index]}
-      />
-   ]);
+  });
